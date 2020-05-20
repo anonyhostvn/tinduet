de068424fc52d8067da65232b7d9fce63dbcae16
Long Nguyễn <longnh07.aigrouplab@gmail.com>
2020-05-20 11:52:54 +0700

    add sidebar

--- src/Layout/BasicLayout/basicLayout.style.js
+++ src/Layout/BasicLayout/basicLayout.style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const BasicLayoutWrapper = styled.div`
   height: 100vh;
   display: flex;
+  font-family: Balsami,serif;
 `;
 
 export const ContentWrapper = styled.div`
--- src/Layout/BasicLayout/component/Sidebar/sidebar.style.js
+++ src/Layout/BasicLayout/component/Sidebar/sidebar.style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
 
 export const SidebarWrapper = styled.div`
+  height: 100vh;
 `;
